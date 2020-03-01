1457d2ca19745797ba92a72e6f8df938b6955668
KJunseo <max9106@naver.com>
2020-03-02 02:57:20 +0900

    BoardDTO

---
+++ src/main/java/com/junseo/board/dto/BoardDto.java
@@ -0,0 +1,39 @@
+package com.junseo.board.dto;
+
+import com.junseo.board.domain.entity.Board;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class BoardDto {
+    private Long id;
+    private String writer;
+    private String title;
+    private String content;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
+    public Board toEntity() {
+        Board build = Board.builder()
+                .id(id)
+                .writer(writer)
+                .title(title)
+                .content(content)
+                .build();
+        return build;
+    }
+
+    @Builder
+    public BoardDto(Long id, String title, String content, String writer, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+        this.id = id;
+        this.writer = writer;
+        this.title = title;
+        this.content = content;
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
+    }
+}
