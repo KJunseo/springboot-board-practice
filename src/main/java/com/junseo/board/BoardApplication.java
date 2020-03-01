59d05e21b7044601cfc2e8b7ac04a6310577cec7
KJunseo <max9106@naver.com>
2020-03-02 02:55:52 +0900

    Entity & Repository

--- src/main/java/com/junseo/board/BoardApplication.java
+++ src/main/java/com/junseo/board/BoardApplication.java
@@ -2,7 +2,9 @@ package com.junseo.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BoardApplication {
 
---
+++ src/main/java/com/junseo/board/domain/entity/Board.java
@@ -0,0 +1,38 @@
+package com.junseo.board.domain.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Board extends TimeEntity {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(length = 10, nullable = false)
+    private String writer;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Builder
+    public Board(Long id, String title, String content, String writer) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+    }
+
+}
