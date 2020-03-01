4aeee7a0bfe5e7b15b3d17254fb98dcdd441a439
KJunseo <max9106@naver.com>
2020-03-02 02:54:33 +0900

    handler 구현

---
+++ src/main/java/com/junseo/board/controller/BoardController.java
@@ -0,0 +1,33 @@
+package com.junseo.board.controller;
+
+import com.junseo.board.dto.BoardDto;
+import com.junseo.board.service.BoardService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class BoardController {
+
+    private BoardService boardService;
+
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    @GetMapping("/")
+    public String list() {
+        return "board/list.html";
+    }
+
+    @GetMapping("/post")
+    public String write() {
+        return "board/write.html";
+    }
+
+    @PostMapping("/post")
+    public String write(BoardDto boardDto) {
+        boardService.savePost(boardDto);
+        return "redirect:/";
+    }
+}
