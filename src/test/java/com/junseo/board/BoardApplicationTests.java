b642e639997d91ee0e7ed1dbb4e6ce05e7ae5eab
KJunseo <max9106@naver.com>
2020-03-02 00:51:31 +0900

    initial & JPA properties

---
+++ src/test/java/com/junseo/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junseo.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
