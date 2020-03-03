654e3869c546cb2fd3ee78ccebfc24cbd1a1dcf0
KJunseo <max9106@naver.com>
2020-03-04 02:46:20 +0900

    게시글 검색 기능

--- src/main/java/com/junseo/board/domain/repository/BoardRepository.java
+++ src/main/java/com/junseo/board/domain/repository/BoardRepository.java
@@ -3,5 +3,8 @@ package com.junseo.board.domain.repository;
 import com.junseo.board.domain.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findByTitleContaining(String keyword);
 }
--- src/main/java/com/junseo/board/service/BoardService.java
+++ src/main/java/com/junseo/board/service/BoardService.java
@@ -64,4 +64,28 @@ public class BoardService {
     public void deletePost(Long id) {
         boardRepository.deleteById(id);
     }
+
+    @Transactional
+    public List<BoardDto> searchPosts(String keyword) {
+        List<Board> boards = boardRepository.findByTitleContaining(keyword);
+        List<BoardDto> boardDtoList = new ArrayList<>();
+
+        if(boards.isEmpty()) return boardDtoList;
+
+        for(Board board : boards) {
+            boardDtoList.add(this.convertEntityToDto(board));
+        }
+
+        return boardDtoList;
+    }
+
+    private BoardDto convertEntityToDto(Board board) {
+        return BoardDto.builder()
+                .id(board.getId())
+                .title(board.getTitle())
+                .content(board.getContent())
+                .writer(board.getWriter())
+                .createdDate(board.getCreatedDate())
+                .build();
+    }
 }
